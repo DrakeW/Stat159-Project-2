64118b72ec833459bd93a03920a54725eb87f048
DrakeW <junyuw@berkeley.edu>
2016-10-20 23:36:46 -0700

    fixed id column issue

--- code/scripts/data-processing-script.R
+++ code/scripts/data-processing-script.R
@@ -5,6 +5,6 @@ temp_credit <- model.matrix(Balance ~ ., data = credit_data)
 new_credit <- cbind(temp_credit[ ,-1], Balance = credit_data$Balance)
 
 # mean centering & standardization
-scaled_credit <- scale(new_credit, center = TRUE, scale = TRUE)
+scaled_credit <- scale(new_credit[, -1], center = TRUE, scale = TRUE)
 write.csv(scaled_credit, file = "data/scaled-credit.csv")
 
