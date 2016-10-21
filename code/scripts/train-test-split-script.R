6e06da204f65e3966519c12db4a289509a3faecb
DrakeW <junyuw@berkeley.edu>
2016-10-21 00:53:57 -0700

    added train test split script

---
+++ code/scripts/train-test-split-script.R
@@ -0,0 +1,11 @@
+credit_data <- read.csv("data/scaled-credit.csv")
+
+set.seed(100)
+train_set_indices <- sample(1:400, 300)
+test_set_indices <- -1 * train_set_indices
+
+train_set <- credit_data[train_set_indices,]
+test_set <- credit_data[test_set_indices,]
+
+write.csv(train_set, file = "data/train-data.csv")
+write.csv(test_set, file = "data/test-data.csv")
