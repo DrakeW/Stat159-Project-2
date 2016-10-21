c0e83a5a7849e0031b80cb25686d2b1ad5528970
DrakeW <junyuw@berkeley.edu>
2016-10-21 01:33:33 -0700

    finished ridge regression model

--- code/scripts/train-test-split-script.R
+++ code/scripts/train-test-split-script.R
@@ -4,8 +4,8 @@ set.seed(100)
 train_set_indices <- sample(1:400, 300)
 test_set_indices <- -1 * train_set_indices
 
-train_set <- credit_data[train_set_indices,]
-test_set <- credit_data[test_set_indices,]
+train_set <- credit_data[train_set_indices,][,-1]
+test_set <- credit_data[test_set_indices,][,-1]
 
 write.csv(train_set, file = "data/train-data.csv")
 write.csv(test_set, file = "data/test-data.csv")
