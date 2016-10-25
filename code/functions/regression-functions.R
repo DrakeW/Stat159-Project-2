def6596b660520bbcd4c5ef014bc823e278d05d6
nikkirethyo <nikkireth@berkeley.edu>
2016-10-24 22:07:32 -0700

    added regression functions

---
+++ code/functions/regression-functions.R
@@ -0,0 +1,28 @@
+#residual sum of squares 
+residual_sum_squares <- function(lm){
+  return(sum(lm$residuals^2))
+}
+
+#total sum of squares 
+total_sum_squares <- function(lm){
+  return(sum(((lm$fitted.values+lm$residuals)-mean(lm$fitted.values+lm$residuals))^2))
+}
+
+#r squared 
+r_squared <- function(lm){
+  return(1-(residual_sum_squares(lm)/total_sum_squares(lm)))
+}
+
+#residual standard error 
+residual_std_error <- function(lm){
+  return(sqrt((1/((length(lm$fitted.values))-(length(lm$coefficients)-1)-1)*residual_sum_squares(lm))))
+}
+
+#f statistic 
+f_statistic <- function(lm){
+  TSS <- total_sum_squares(lm)
+  RSS <- residual_sum_squares(lm)
+  n <- length(lm$fitted.values)
+  p <- length(lm$coefficients) - 1
+  return(((TSS - RSS)/p)/(RSS/(n-p-1)))
+}
