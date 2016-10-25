e41f0ac66f7822b9f4303f25c25477a2b48a30b1
nikkirethyo <nikkireth@berkeley.edu>
2016-10-25 13:22:53 -0700

    used regression functions to investigate OLS fit

--- code/scripts/ols-regression-script.R
+++ code/scripts/ols-regression-script.R
@@ -3,6 +3,9 @@ train_data <- read.csv("data/train-data.csv")
 test_data <- read.csv("data/test-data.csv")
 full_data <- read.csv("data/scaled-credit.csv")
 
+#load in RData for regression functions 
+load("data/regression-functions.Rdata")
+
 # train model based on train data
 ols.mod <- lm(Balance~., data = train_data[,-1])
 # predict on test set
@@ -14,4 +17,9 @@ ols_mse <- mean((ols.pred - target_y)^2)
 ols_official_fit <- lm(Balance~., data = full_data[,-1])
 ols_coeff <- coef(ols_official_fit)
 
-save(ols.mod, ols_mse, ols_official_fit, ols_coeff, file = "data/regressions/ols-model.RData")
+#investigate the models fit 
+ols_summary <- data.frame(residual_sum_squares(ols_official_fit), total_sum_squares(ols_official_fit), r_squared(ols_official_fit), residual_std_error(ols_official_fit), f_statistic(ols_official_fit))
+rownames(ols_summary) <- "OLS"
+colnames(ols_summary) <- c("RSS", "TSS", "R Squared", "Residual Std. Error", "F Stat")
+
+save(ols.mod, ols_mse, ols_official_fit, ols_coeff, ols_summary, file = "data/regressions/ols-model.RData")
